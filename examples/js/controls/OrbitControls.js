df7f7bae7c81e039fe1c680ed3d180bd12b5678f
Remi <40400644+Remi-Tribia@users.noreply.github.com>
2018-11-19 16:49:10 +0100

    Update OrbitControls.js

--- examples/js/controls/OrbitControls.js
+++ examples/js/controls/OrbitControls.js
@@ -244,6 +244,10 @@ THREE.OrbitControls = function ( object, domElement ) {
 	// Accès à state depuis l'extérieur
 	// this.getState = function () { return state; };
 	// this.getPreviousState = function () { return previousState; };
+	this.getSpherical = function () { return spherical; }; // DEBUG MODIF
+	this.updateMaxPolarAngle = function (_maxPolarAngle) {
+		this.maxPolarAngle = _maxPolarAngle;
+	};
 	this.enable = function () {
 
 		this.enabled = true;
